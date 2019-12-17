fdf4981abe49212148438aa6f7767efe61eca367
Hsins <hsinspeng@gmail.com>
2019-12-17 12:24:45 +0800

    Finish "[8 kyu] Alan Partridge II - Apple Turnover" in Python.

---
+++ Python/8 kyu/Alan Partridge II - Apple Turnover/solution.py
@@ -0,0 +1,8 @@
+# [8 kyu] Alan Partridge II - Apple Turnover
+#
+# Author:   Hsins
+# Date:     2019/12/17
+
+
+def apple(x):
+    return "It's hotter than the sun!!" if int(x) ** 2 > 1000 else "Help yourself to a honeycomb Yorkie for the glovebox."
