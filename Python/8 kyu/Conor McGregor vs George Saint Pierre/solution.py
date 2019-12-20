c7634529a7fa0e947ad7d8eba531978a7da557d0
Hsins <hsinspeng@gmail.com>
2019-12-20 17:36:33 +0800

    Finish "[8 kyu] Conor McGregor vs George Saint Pierre" in Python.

---
+++ Python/8 kyu/Conor McGregor vs George Saint Pierre/solution.py
@@ -0,0 +1,8 @@
+# [8 kyu] Conor McGregor vs George Saint Pierre
+#
+# Author:   Hsins
+# Date:     2019/12/20
+
+
+def quote(fighter):
+    return 'I am not impressed by your performance.' if fighter.upper() == 'GEORGE SAINT PIERRE' else 'I\'d like to take this chance to apologize.. To absolutely NOBODY!'
